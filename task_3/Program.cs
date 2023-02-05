7e57bc0a2a98245c12d7d12161e45d9f25a2c4f7
Evgeniy Nelidin <nelidin@yandex.ru>
2023-02-05 15:05:25 +0300

    add task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+
+Console.Clear();
+
+int N = new Random().Next(1, 50);
+Console.WriteLine($"N = {N}");
+int number = 1;
+
+while (number <= N)
+{
+    int cube = number * number * number;
+
+    if (cube % 2 == 0)
+
+    {
+        Console.Write($"{cube}, ");
+    }
+
+    number++;
+}
