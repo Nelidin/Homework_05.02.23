85b75393123a3f8003bc319a49c0a03c7d3d4073
Evgeniy Nelidin <nelidin@yandex.ru>
2023-02-05 14:06:03 +0300

    add task_1

---
+++ task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int summ = 0;
+
+while (num != 0)
+{
+    summ = summ + num % 10;
+    num = num / 10;
+}
+Console.WriteLine($"Сумма цифр = {summ}");
