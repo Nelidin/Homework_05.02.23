a5983c70ea962410a9a12db553fc0f5a191f553c
Evgeniy Nelidin <nelidin@yandex.ru>
2023-02-05 14:26:24 +0300

    add task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int result = 1;
+
+for (int i = 1; i <= N; i++)
+{
+    result = result * i;
+}
+Console.WriteLine($"произведение чисел = {result}");
